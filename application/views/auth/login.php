60af024cae7ae6b3a2ced0789820b8dde724a3ca
NizarRestu <nizarrestuaji18@gmail.com>
2023-11-02 08:24:10 +0700

    update

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -236,7 +236,7 @@
                     <header class="subHeader">Selamat Datang di <b>Website Sewa Ruang</b> Silahkan lengkapi data anda
                     </header>
 
-                    <form action="" method="post">
+                    <form action="<?php echo base_url('auth/aksi_login')?>" method="post">
                         <div class="inputContainer">
                             <label class="label" for="emailAddress"><img src="https://i.imgur.com/Hn13wvm.png"
                                     class="labelIcon"><span>Email*
