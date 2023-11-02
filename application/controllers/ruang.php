cd58ab15234a5ea9a0dc1f8a2e44464f6254ffb5
vanioersa <vanvanramadhan@gmail.com>
2023-11-02 14:42:56 +0700

    membuat fungsi tampil image

---
+++ application/controllers/ruang.php
@@ -0,0 +1,60 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Ruang extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_model');
+        $this->load->helper('my_helper');
+    }
+
+    public function index()
+    {
+        $data['ruang'] = $this->m_model->get_data('ruangan')->result();
+        $id = 1; // Ganti dengan ID yang sesuai
+        $data['ruang'] = $this->m_model->get_gambar_ruangan($id);
+        $this->load->view('ruang/Data_Ruangan', $data);
+    }
+
+    public function tambah_ruangan()
+{
+    // Lakukan operasi yang sesuai, seperti memuat halaman "tambah ruang"
+    $this->load->view('ruang/tambah_ruang');
+}
+
+
+    public function akis_tambah_ruangan()
+    {
+        // Mengambil data dari POST request
+        $no_lantai = $this->input->post('no_lantai');
+        $no_ruang = $this->input->post('no_ruang');
+
+        // Menyiapkan data yang akan dimasukkan ke database
+        $data = array(
+            'no_lantai' => $no_lantai,
+            'no_ruang' => $no_ruang
+        );
+
+        // Memasukkan data ke dalam tabel 'ruangan' di database Anda
+        $inserted = $this->m_model->insert_data('ruangan', $data);
+
+        if ($inserted) {
+            // Data berhasil dimasukkan, tampilkan SweetAlert2 pesan sukses dan redirect
+            echo '<script type="text/javascript">
+                Swal.fire("Sukses!", "Data berhasil dimasukkan.", "success").then(function() {
+                    window.location = "' . site_url('data_master_pelanggan') . '"; 
+                });
+            </script>';
+        } else {
+            // Penyisipan gagal, tampilkan SweetAlert2 pesan error dan redirect
+            echo '<script type="text/javascript">
+                Swal.fire("Gagal!", "Gagal memasukkan data. Silakan coba lagi.", "error").then(function() {
+                    window.location = "' . site_url('Data_Ruangan') . '";
+                });
+            </script>';
+        }
+    }
+}
---
+++ application/models/m_model.php
@@ -0,0 +1,37 @@
+<?php
+class M_model extends CI_Model
+{
+    function get_data($table)
+    {
+        return $this->db->get($table);
+    }
+
+    function getwhere($table, $data)
+    {
+        return $this->db->get_where($table, $data);
+    }
+
+    public function delete($table, $field, $id)
+    {
+        $data = $this->db->delete($table, array($field => $id));
+        return $data;
+    }
+
+    public function tambah_data($table, $data)
+    {
+        $this->db->insert($table, $data);
+        return $this->db->insert_id($table);
+    }
+
+    public function get_by_id($tabel, $id_column, $id)
+    {
+        $data = $this->db->where($id_column, $id)->get($tabel);
+        return $data;
+    }
+
+    public function get_gambar_ruangan($id) {
+        // Gantilah 'ruangan' dengan nama tabel yang sesuai di database Anda
+        $query = $this->db->where('id', $id)->get('ruangan');
+        return $query->row(); // Menggunakan row() untuk mengambil satu data
+    }
+}
