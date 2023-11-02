06a707d9bd59d963f48f169b01c9afe2e5269e2c
AndiSaputro50 <AndiSaputro50>
2023-11-02 08:49:54 +0700

    update

--- application/controllers/Pelanggan.php
+++ application/controllers/Pelanggan.php
@@ -11,20 +11,20 @@ class Admin extends CI_Controller {
 
 }
 
-public function admin()
+public function pelanggan()
 {
     $data['pelanggan'] = $this->m_model->get_data('pelanggan')->result();
-    $this->load->view('Admin/data_master_pelanggan', $data);
+    $this->load->view('pelanggan/data_master_pelanggan', $data);
 }
 public function tambah_pelanggan()
 {
     $data['pelanggan'] = $this->m_model->get_data('pelanggan')->result();
-    $this->load->view('admin/tambah_pelanggan');
+    $this->load->view('pelanggan/tambah_pelanggan');
 }
 public function aksi_tambah_pelanggan()
 {
     $this->m_model->tambah_data('pelanggan', $data);
-    redirect(base_url('admin/pelanggan'));
+    redirect(base_url('pelanggan/pelanggan'));
 }
 
 }
