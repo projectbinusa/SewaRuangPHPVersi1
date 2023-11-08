014585ae975e65c9e106b7e76e6e874437143756
vanioersa <vanvanramadhan@gmail.com>
2023-11-09 06:35:12 +0700

    update

--- application/controllers/ruang.php
+++ application/controllers/ruang.php
@@ -109,4 +109,151 @@ class Ruang extends CI_Controller
         echo json_encode($response);
     }
     
+    public function edit_ruangan($id)
+    {
+        // Ambil data ruangan dari database berdasarkan $id
+        $data['ruangan'] = $this->m_model->get_ruangan_by_id($id);
+        $this->load->view('ruang/edit_ruangan', $data);
+    }
+
+    public function aksi_edit_ruangan($id)
+    {
+        $id = $id; // ID ruangan yang diberikan melalui parameter
+
+        // Ambil data yang di-post dari form
+        $no_lantai = $this->input->post('no_lantai');
+        $no_ruang = $this->input->post('no_ruang');
+        $deskripsi = $this->input->post('deskripsi');
+        $image = $_FILES['foto']['name'];
+        $foto_temp = $_FILES['foto']['tmp_name'];
+
+        // Inisialisasi pesan respons
+        $response = [
+            'status' => 'error', // Default status error
+            'message' => 'Terjadi kesalahan saat mengubah ruangan.', // Default pesan error
+            'redirect' => '' // Redirect URL setelah berhasil atau gagal
+        ];
+
+        // Cek apakah ID ruangan valid (contoh: Anda dapat menambahkan pengecekan di sini)
+        if ($id) {
+            // Dapatkan data ruangan dari database berdasarkan ID
+            $current_data = $this->m_model->get_ruangan_by_id($id);
+
+            if ($current_data) {
+                // Cek apakah ada perubahan data
+                $data = [];
+
+                // Set data ke dalam array tanpa memeriksa perubahan
+                $data['no_lantai'] = $no_lantai;
+                $data['no_ruang'] = $no_ruang;
+                $data['deskripsi'] = $deskripsi;
+
+                // Jika ada foto yang diunggah
+                if (!empty($image)) {
+                    // Lakukan pengelolaan gambar seperti yang telah dijelaskan dalam pertanyaan sebelumnya
+                    $kode = round(microtime(true) * 100);
+                    $file_name = $kode . '_' . $image;
+                    $upload_path = './image/ruangan/' . $file_name;
+
+                    if (move_uploaded_file($foto_temp, $upload_path)) {
+                        // Hapus image lama jika ada
+                        $old_file = $this->m_model->get_image_by_id('ruangan', $id);
+                        if ($old_file && file_exists('./image/ruangan/' . $old_file)) {
+                            unlink('./image/ruangan/' . $old_file);
+                        }
+
+                        $data['image'] = $file_name;
+
+                        // Operasi berhasil
+                        $response = [
+                            'status' => 'success',
+                            'message' => 'Berhasil Mengubah Ruangan',
+                            'redirect' => base_url('ruang'), // Redirect ke halaman daftar ruangan jika berhasil
+                        ];
+                    } else {
+                        // Gagal mengunggah gambar baru
+                        $response = [
+                            'status' => 'error',
+                            'message' => 'Gagal mengunggah gambar. Silakan coba lagi.',
+                            'redirect' => base_url('ruang/edit_ruangan/' . $id), // Redirect ke halaman edit jika gagal
+                        ];
+                    }
+                } else {
+                    // Validasi minimal satu data acak
+                    $response = [
+                        'status' => 'error',
+                        'message' => 'Anda harus mengunggah setidaknya satu data acak (gambar).',
+                        'redirect' => base_url('ruang/edit_ruangan/' . $id), // Redirect ke halaman edit jika validasi gagal
+                    ];
+                }
+
+                // Eksekusi dengan model edit_ruangan tanpa memeriksa perubahan
+                $update_result = $this->m_model->edit_ruangan($id, $data);
+
+                if ($update_result) {
+                    $response = [
+                        'status' => 'success',
+                        'message' => 'Berhasil Mengubah Ruangan',
+                        'redirect' => base_url('ruang'), // Redirect ke halaman daftar ruangan jika berhasil
+                    ];
+                }
+            }
+        }
+
+        // Kirim respons JSON
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+
+    public function hapus_image($id)
+    {
+        // Ambil data ruangan dari database berdasarkan ID
+        $ruangan = $this->m_model->get_ruangan_by_id($id);
+
+        if ($ruangan) {
+            // Dapatkan nama file gambar
+            $image_file = $ruangan->image;
+
+            if ($image_file) {
+                // Hapus file gambar dari direktori
+                $image_path = './image/ruangan/' . $image_file;
+
+                if (file_exists($image_path) && unlink($image_path)) {
+                    // Update data ruangan di database untuk menghapus referensi gambar
+                    $data = ['image' => '']; // Atur kolom gambar menjadi kosong
+                    $this->m_model->edit_ruangan($id, $data);
+
+                    // Kirim respons JSON untuk memberi tahu hasil penghapusan gambar
+                    $response = [
+                        'status' => 'success',
+                        'message' => 'Gambar telah dihapus.',
+                        'redirect' => base_url('ruang') // Tambahkan URL tujuan
+                    ];
+                } else {
+                    // Jika gagal menghapus gambar
+                    $response = [
+                        'status' => 'error',
+                        'message' => 'Gambar tidak dapat dihapus. Silakan coba lagi.'
+                    ];
+                }
+            } else {
+                // Jika tidak ada gambar
+                $response = [
+                    'status' => 'error',
+                    'message' => 'Gambar tidak ditemukan.'
+                ];
+            }
+        } else {
+            // Jika data ruangan tidak ditemukan
+            $response = [
+                'status' => 'error',
+                'message' => 'Data ruangan tidak ditemukan.'
+            ];
+        }
+
+        // Kirim respons JSON ke browser
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
 }
