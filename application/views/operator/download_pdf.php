64693942ce61b0686d8380e939e3cb6f068f87be
yuniarahmah <yuraama75@gmail.com>
2023-11-13 15:56:47 +0700

    update

--- application/views/operator/download_pdf.php
+++ application/views/operator/download_pdf.php
@@ -4,77 +4,60 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html>
+<head>
     <style>
-        .header h2{
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.5;
+        }
+
+        .header {
             text-align: center;
-            background: #f2f2f2;
-        }
-
-        .invoice {
-            float: right;
-            font-weight: bold;
-            color: #705D56; 
-        }
-
-        .invoice-container {
-            margin-top: 30px;
             padding: 20px;
-            background: #FFFFFF;
-            border: 1px solid #D1CDC6;
-            border-radius: 8px;
+            background: #3876BF;
+            color: white;
         }
 
-        .invoice h2 {
-            color: #705D56;
-            font-size: 24px;
-            font-weight: bold;
-            text-align: left;
+        .invoice-yu {
+            text-align: center;
+            margin-bottom: 20px;
         }
 
         .invoice-details {
-            margin-top: 20px;
-            font-size: 14px;
-            font-weight: bold;
+            margin-bottom: 20px;
         }
 
         .invoice-details table {
             width: 100%;
+            margin-bottom: 20px;
         }
 
-        .invoice-details table tr td {
-            padding: 8px;
-            border-left: px             
+        .invoice-details table th,
+        .invoice-details table td {
+            padding: 5px;
+        }
+
+        .invoice-details table th {
+            background: #f2f2f2;
+            text-align: left;
+        }
+
+        .item-table {
+            width: 100%;
+            border-collapse: collapse;
         }
 
         .item-table th,
         .item-table td {
-            padding: 10px;
-            text-align: center;
-            border-right: 1px solid #D1CDC6; /* Menambah garis vertikal pada td */
-        }
-
-        .item-table td:last-child {
-            border-right: none; /* Menghapus garis vertikal pada td terakhir */
+            padding: 8px;
+            text-align: left;
+            /* border-bottom: 1px solid #ddd; */
         }
 
         .item-table th {
-            border-top: 2px solid #705D56;
-            border-bottom: 2px solid #705D56;
-            margin: 0;
-            padding: 10px;
-            color: #0C356A;
-            border-left: 0px ;              
-        }
-
-        .item-table td::before {
-            content: "";
-            display: block;
-            text-align: center;
-            margin-bottom: 5px;
-        }
-
-        .item-table tbody tr:last-child td {
-            border-bottom: none;
+            background: #f2f2f2;
+            font-weight: bold;
         }
 
         .total {
@@ -90,18 +73,21 @@
             font-size: 14px;
             font-weight: bold;
             background: linear-gradient(to right, #f2f2f2, transparent);
-            padding: 20px;
-            color: ; /* Tambahkan warna yang diinginkan */
-            border-left: 20px solid #0C356A ; /* Lebar dan warna garis vertikal */
-            margin-left: 0; /* Hapus margin kiri */
+            color: #0C356A;
+            border-left: 18px solid #0C356A;
+            margin-left: 0;
+            height: 30%; /* Set a large value for height */
         }
 
         .payment-info h4 {
-            margin-bottom: 10px;
+            /* margin-bottom: 10%; */
+            margin-left: 18px; /* Set margin-left to match the border-left width */
         }
 
         .payment-info p {
+            margin-bottom: 10%;
             margin: 5px 0;
+            margin-left: 18px; /* Set margin-left to match the border-left width */
         }
     </style>
 
@@ -110,13 +96,14 @@
 <body>
 
     <div class="header">
+        <img src="<?php echo base_url('image/logo.png') ?>" alt="Logo" style="width: 2px;">
         <h2>RuangSewa.com</h2>
     </div>
 
         <div class="invoice-yu">
             <h2>Bukti Pembayaran <span class="invoice">INVOICE</span></h2>
         </div>
-
+<hr>
         <div class="invoice-details">
             <table>
                 <tr>
@@ -130,24 +117,25 @@
             </table>
         </div>
 <br><br>
-        <table rules="cols" class="item-table hover:table-fixed" id="itemTable">
+        <table rules="cols" class="item-table hover:table-fixed">
             <thead>
                 <tr>
                     <th>No</th>
                     <th>Ruangan/Lantai</th>
                     <th>Kode Booking</th>
+                    <th>Snack</th>
                     <th>Status</th>
                     <th>total</th>
                 </tr>
             </thead>
-            <tbody>
-                <?php $no = 1; foreach ($bukti as $key): { ?>
+            <tbody >
+                <?php $no = 1; foreach ($ruang as $key): { ?>
                     <tr>
-                        <td><?php echo $no++; ?></td>
                         <td></td>
-                        <td><?php echo $key->no_lantai; ?></td>
-                        <td><?php echo $key->no_ruang; ?></td>
-                        <td>free</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                         <td></td>
                     </tr>
                 <?php } ?>
@@ -161,10 +149,10 @@
 <br><br>
         <div class="payment-info">
             <h4>PAYMENT INFO</h4>
-            <p>Bank         <span style="margin-left: 74px;">:</span></p>
-            <p>Account Name <span style="margin-left: 17px;">:</span></p>
-            <p>Account No   <span style="margin-left: 35px;">:</span></p>
-            <p>Payment by   <span style="margin-left: 35px;">:</span></p>
+            <p>Bank         <span class="info">:</span></p>
+            <p>Account Name <span class="info">:</span></p>
+            <p>Account No   <span class="info">:</span></p>
+            <p>Payment by   <span class="info">:</span></p>
         </div>
     </div>
 
