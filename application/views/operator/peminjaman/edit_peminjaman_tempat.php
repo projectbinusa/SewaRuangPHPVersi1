9f87d53f1b3cb71c0fa7bd1ad6d2151a83ae7aee
NizarRestu <nizarrestuaji18@gmail.com>
2023-11-16 14:48:51 +0700

    update

--- application/views/operator/peminjaman/edit_peminjaman_tempat.php
+++ application/views/operator/peminjaman/edit_peminjaman_tempat.php
@@ -205,7 +205,7 @@
                 <div class="green-bar"></div>
                 <h1 id="title" class="main-heading">Form Edit Peminjaman</h1>
             </header>
-            <?php foreach ($peminjaman as $row): ?>
+            <?php foreach ($peminjaman as $row):  $tanggal_booking = $row->tanggal_booking; $tanggal_berakhir = $row->tanggal_berakhir;?>
                 <form action="<?php echo base_url('operator/aksi_edit_peminjaman') ?>" method="post" id="survey-form"
                     class="survey-form">
                     <input type="hidden" name="id" id="nama" class="nama" value="<?php echo $row->id ?>">
@@ -242,14 +242,12 @@
 
                     </datalist>
 
-                    <label for="total_booking" id="total_booking-label">Booking Dari Tanggal<span
-                            class="required">*</span></label>
-                    <input type="date" name="booking" id="total_booking" class="total_booking"
-                        value="<?php echo $row->tanggal_booking ?>" required>
-                    <label for="total_booking" id="total_booking-label">Booking Sampai Tanggal<span
-                            class="required">*</span></label>
-                    <input type="date" name="akhir_booking" id="total_booking" class="total_booking"
-                        value="<?php echo $row->tanggal_berakhir ?>" required>
+                    <label for="booking" id="booking-label">Booking Dari Tanggal<span class="required">*</span></label>
+<input type="date" name="booking" id="booking" class="total_booking" value="<?php echo $tanggal_booking  ?>" required>
+
+<label for="akhir_booking" id="akhir_booking-label">Booking Sampai Tanggal<span class="required">*</span></label>
+<input type="date" name="akhir_booking" id="akhir_booking" class="total_booking" value="<?php echo $tanggal_berakhir ?>" required>
+
                     <input type="submit" id="submit" class="submit" value="Submit">
                 </form>
             <?php endforeach ?>
