e6823f5a16ddd62a75ec630ffeb55cfd91db5877
yuniarahmah <yuraama75@gmail.com>
2023-11-18 12:25:07 +0700

    update

--- application/controllers/Operator.php
+++ application/controllers/Operator.php
@@ -699,18 +699,6 @@ class operator extends CI_Controller
         
         $spreadsheet = new Spreadsheet();
 
-	
-
-
-
-
-
-
-
-
-
-
-
     public function tambahan(){
         $data['tambahan'] = $this->m_model->get_data('tambahan')->result();
         $this->load->view('operator/tambahan/tambahan',$data);
