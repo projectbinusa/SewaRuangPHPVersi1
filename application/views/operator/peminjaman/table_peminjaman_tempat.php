3b1b8cb02d009b7531ce68b0e70dc2c73f54bc01
yuniarahmah <yuraama75@gmail.com>
2023-11-28 15:52:43 +0700

    update

--- application/views/operator/peminjaman/table_peminjaman_tempat.php
+++ application/views/operator/peminjaman/table_peminjaman_tempat.php
@@ -47,6 +47,7 @@
         body {
             font-family: "DM Sans", sans-serif;
             background-color: #f1f1f1;
+            min-height: screen;
             position: relative;
         }
 
@@ -891,11 +892,6 @@
             background-color: transparent;
             color: #222;
         }
-
-        .contain-all {
-            overflow-y: scroll;
-            height: 200vh;
-        }
     </style>
     <!-- script modal -->
     <script>
@@ -905,19 +901,20 @@
     </script>
 </head>
 
-<body class="relative h-screen overflow-hidden">
+<body class="relative min-h-screen overflow-hidden">
     <?php $this->load->view('sidebar'); ?>
-    <main class="mx-4 my-8">
-        <div class="container mx-auto my-8">
+    <main class="contain-all max-h-screen overflow-y-auto">
+        <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-8 text-center">
                     <div class="section-title">
                         <h2 class="title">Peminjaman Tempat</h2>
+
                     </div>
                 </div>
             </div>
         </div>
-        <div class="w-full lg:w-10/12 mx-auto p-4">
+        <div class="container-table row justify-content-center">
 
             <div class="col-lg-12 p-12">
                 <div class="header-item">
@@ -1047,34 +1044,30 @@
                 </div>
             </div>
         </div>
-
-
         </div>
-        </section>
+    </main>
 
+    <!-- modal -->
+    <div class="fixed z-10 overflow-y-auto top-0 w-full left-0 hidden" id="modal">
+        <div class="flex items-center justify-center min-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div class="fixed inset-0 transition-opacity">
+                <div class="absolute inset-0 bg-gray-900 opacity-75">
+                </div>
+                <span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
+                <div class="inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <label class="font-medium text-gray-800">File</label>
+                        <input type="file" class="w-full outline-none rounded bg-gray-100 p-2 mt-2 mb-3" />
 
-        <!-- modal -->
-        <div class="fixed z-50 overflow-y-auto top-0 w-full left-0 hidden" id="modal">
-            <div class="flex items-center justify-center min-height-200vh pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                <div class="fixed inset-0 transition-opacity">
-                    <div class="absolute inset-0 bg-gray-900 opacity-75">
                     </div>
-                    <span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
-                    <div class="inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-                        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                            <label class="font-medium text-gray-800">File</label>
-                            <input type="file" class="w-full outline-none rounded bg-gray-100 p-2 mt-2 mb-3" />
-
-                        </div>
-                        <div class="bg-gray-200 px-4 py-3 text-right">
-                            <button type="button" class="py-2 px-4 bg-red-500 text-white rounded hover:bg-red-700 mr-2" onclick="toggleModal()"> Batal</button>
-                            <button type="button" class="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700 mr-2">Impor</button>
-                        </div>
+                    <div class="bg-gray-200 px-4 py-3 text-right">
+                        <button type="button" class="py-2 px-4 bg-red-500 text-white rounded hover:bg-red-700 mr-2" onclick="toggleModal()"> Batal</button>
+                        <button type="button" class="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700 mr-2">Impor</button>
                     </div>
                 </div>
             </div>
         </div>
-    </main>
+    </div>
 
     <!-- jQuery -->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
