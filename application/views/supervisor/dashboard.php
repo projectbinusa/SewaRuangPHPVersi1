a1658da87d7e90f7fd97e8d8c9898b9e222d7339
melyanahasya <melysasa49@gmail.com>
2023-12-02 07:57:26 +0700

    post

--- application/views/supervisor/dashboard.php
+++ application/views/supervisor/dashboard.php
@@ -5753,11 +5753,11 @@
                 <div class="container px-4 mx-auto">
 
                     <div class="md:flex md:-mx-4 md:items-center">
-                        <div class="md:flex-1 md:px-4 text-center md:text-left">
-                            <!-- <p class="text-white">&copy; <strong>FWR</strong></p> -->
-                        </div>
+                        <!-- <div class="md:flex-1 md:px-4 text-center md:text-left">
+                            <p class="text-white">&copy; <strong>FWR</strong></p>
+                        </div> -->
                         <div class="md:flex-1 md:px-4 text-center md:text-right">
-                            <a href="#" class="py-2 px-4 text-white inline-block hover:underline">© 2023 Sewaruang. All rights reserved.</a>
+                            <a href="" class="py-2 px-4 text-white inline-block hover:underline">© 2023 Sewaruang. All rights reserved.</a>
                         </div>
                     </div>
 
